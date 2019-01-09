9d54f8fc71271c6ed89f7b583e221507466bfd0b
edtamarin <egor@tamarin.eu>
2019-01-09 22:26:01 +0100

    OTA update + MQTT integration

--- WeatherStation/src/main.cpp
+++ WeatherStation/src/main.cpp
@@ -5,8 +5,19 @@
 #include <WiFiClient.h>
 #include <PubSubClient.h>
 #include <Ticker.h>
+#include <ESP8266httpUpdate.h>
 
-// NETWORK SETUP HERE
+#define DEVICE "wsta"
+#define FW_VERSION "0_2_1"
+
+const char* ssid = "Convention 48";
+const char* password = "zurich4ccords";
+const char* mqttServer = "192.168.0.111";
+const int mqttPort = 1883;
+const char* mqttUser = "mosqSH";
+const char* mqttPassword = "smartMQTT";
+
+const char* updateUrl = "http://192.168.0.111:1880/update/"DEVICE;
 
 char tempC[16];
 char humi[16];
@@ -27,14 +38,42 @@ Ticker blinker;
 
 int ledPins[2] = {D0,D6};
 
+void runUpdate(){
+  t_httpUpdate_return httpResult;
+  ESPhttpUpdate.rebootOnUpdate(false);
+  httpResult = ESPhttpUpdate.update(updateUrl,FW_VERSION);
+  switch(httpResult){
+    case HTTP_UPDATE_NO_UPDATES:
+      mqttClient.publish("info","No update to weather station required");
+      break;
+    case HTTP_UPDATE_OK:
+      mqttClient.publish("info","Weather station updated");
+      ESP.restart();
+      break;
+    case HTTP_UPDATE_FAILED:
+      mqttClient.publish("info","Weather station update failed");
+      Serial.println(ESPhttpUpdate.getLastErrorString());
+      break;
+    default:
+      mqttClient.publish("info","Weather station not updated");
+      Serial.println(ESPhttpUpdate.getLastErrorString());
+      break;
+  }
+}
+
 void callback(char* topic, byte* payload, unsigned int length) {
+  String messageReceived = "";
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
   for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
+    messageReceived += (char)payload[i];
   }
-  Serial.println();
+  Serial.println(messageReceived);
+  if (messageReceived == "WSTA_update"){
+    mqttClient.publish("info","Weather station requested update");
+    runUpdate();
+  }
 }
 
 void reconnect() {
@@ -45,8 +84,8 @@ void reconnect() {
     if (mqttClient.connect("ShedStation", mqttUser, mqttPassword)) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      mqttClient.publish("shed/gen", "BME280 online");
-      mqttClient.subscribe("gen/stat");
+      mqttClient.publish("info", "Weather station online, version "FW_VERSION);
+      mqttClient.subscribe("control");
     } else {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
